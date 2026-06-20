9d8a54ab8b8e597542964fef7c58630fb5a24465
ayo-ola0710 <israelolatunle2015@gmail.com>
2026-06-20 15:22:41 +0100

    created wallet service

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,12 @@ import { PrismaService } from './prisma/prisma.service';
 import { UserModule } from './user/user.module';
 import { InvoiceModule } from './invoice/invoice.module';
 import { BusinessModule } from './business/business.module';
+import { WalletService } from './wallet/wallet.service';
 
 @Module({
   imports: [UserModule, InvoiceModule, BusinessModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService, PrismaService, WalletService],
   exports: [PrismaService],
 })
 export class AppModule {}
